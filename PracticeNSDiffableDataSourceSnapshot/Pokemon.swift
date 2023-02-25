3d2261a8c5e8e633bd49b0d657b45290a0021d4e
0102johnny <rt2824.recruit.1022@gmail.com>
2023-02-25 17:34:38 +0900

    データModelを定義

---
+++ PracticeNSDiffableDataSourceSnapshot/Pokemon.swift
@@ -0,0 +1,44 @@
+//
+//  Pokemon.swift
+//  PracticeNSDiffableDataSourceSnapshot
+//
+//  Created by Johnny Toda on 2023/02/25.
+//
+
+import Foundation
+
+// ポケモンのデータ構造
+struct Pokemon: Decodable, Hashable {
+    static func == (lhs: Pokemon, rhs: Pokemon) -> Bool {
+        lhs.id == rhs.id
+    }
+    // ポケモンの名前
+    let name: String
+    // ポケモンの図鑑No.
+    let id: Int
+    // ポケモンの画像
+    let sprites: Image
+    // ポケモンのタイプ
+    let types: [TypeEntry]
+}
+
+// 画像のデータ構造
+struct Image: Decodable, Hashable {
+    // ポケモンが正面向きの画像
+    let frontImage: String
+
+    // デコードの際の代替キーをfrontImageプロパティにセット
+    enum CodingKeys: String, CodingKey {
+        case frontImage = "front_default"
+    }
+}
+
+// ポケモンのタイプ
+struct TypeEntry: Decodable, Hashable {
+  let type: Mode
+}
+
+// "Type"が命名で利用できず、他に適切な表現が思い浮かばなかった。
+struct Mode: Decodable, Hashable {
+let name: String
+}
