8365bea535f4c6486b492054d7a1eb063a7dae0e
0102johnny <rt2824.recruit.1022@gmail.com>
2023-02-25 17:48:29 +0900

    一つ目のSectionに表示させるCellのViewを作成

---
+++ PracticeNSDiffableDataSourceSnapshot/PokemonTypeCell.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonTypeCell.swift
+//  PracticeNSDiffableDataSourceSnapshot
+//
+//  Created by Johnny Toda on 2023/02/25.
+//
+
+import UIKit
+
+class PokemonTypeCell: UICollectionViewCell {
+    @IBOutlet private weak var typeLabel: UILabel!
+
+    static let nib = UINib(nibName: String(describing: PokemonTypeCell.self), bundle: nil)
+    static let identifier = String(describing: PokemonTypeCell.self)
+
+    func configure(type: String) {
+        typeLabel.text = type
+    }
+}
