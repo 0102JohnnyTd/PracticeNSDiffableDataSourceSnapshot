5bf583c9154c45a0897cf9231000b26c35abcb74
0102johnny <rt2824.recruit.1022@gmail.com>
2023-02-25 16:57:25 +0900

    Initial commit

---
+++ PracticeNSDiffableDataSourceSnapshot/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  PracticeNSDiffableDataSourceSnapshot
+//
+//  Created by Johnny Toda on 2023/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
