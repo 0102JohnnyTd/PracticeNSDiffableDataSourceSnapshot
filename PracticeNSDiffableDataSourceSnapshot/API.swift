e49af559a859771e61de0feef82135ea315c7163
0102johnny <rt2824.recruit.1022@gmail.com>
2023-02-28 15:43:44 +0900

    API関連の処理をまとめたModelを作成

---
+++ PracticeNSDiffableDataSourceSnapshot/API.swift
@@ -0,0 +1,67 @@
+//
+//  API.swift
+//  PracticeNSDiffableDataSourceSnapshot
+//
+//  Created by Johnny Toda on 2023/02/28.
+//
+
+import Foundation
+
+
+final class API {
+    // 通信によって取得したデータをパース
+    // 取得したポケモンのデータをSwiftの型として扱う為にデコード
+    func decodePokemonData(completion: @escaping (Result<[Pokemon], Error>) -> Void) {
+        print(#function)
+        // データの取得を実行
+        fetchPokemonData(completion: { result in
+            switch result {
+            case .success(let dataArray):
+                var pokemons: [Pokemon] = []
+                dataArray.forEach {
+                    do {
+                        let pokemon = try JSONDecoder().decode(Pokemon.self, from: $0)
+                        pokemons.append(pokemon)
+                    } catch {
+                        completion(.failure(error))
+                    }
+                }
+                completion(.success(pokemons))
+            case .failure(let error as URLError):
+                completion(.failure(error))
+            case .failure(_):
+                fatalError("Unexpected Error")
+            }
+        })
+    }
+
+    // 通信を実行
+    private func fetchPokemonData(completion: @escaping (Result<[Data], Error>) -> Void) {
+        var dataArray: [Data] = []
+        let urls = getURLs()
+        urls.forEach {
+            guard let url = $0 else { return }
+            let task = URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
+                if let error = error {
+                    completion(.failure(error))
+                }
+                if let data = data {
+                    dataArray.append(data)
+                }
+                if urls.count == dataArray.count {
+                    completion(.success(dataArray))
+                }
+            })
+            task.resume()
+        }
+
+    }
+
+    // ポケモン151匹分のリクエストURLを取得
+    private func getURLs() -> [URL?] {
+        let pokeDexRange = 1...151
+        let urls = pokeDexRange.map { URL(string: "https://pokeapi.co/api/v2/pokemon/\($0)/") }
+
+        return urls
+    }
+}
